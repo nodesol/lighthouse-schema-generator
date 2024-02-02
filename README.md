ae67e7cf1c90b4333857d891b8b7638542b11199
Amer Chaudhary <amermchaudry@gmail.com>
2024-02-02 15:51:20 +0500

    +Queries & Mutations

--- README.md
+++ README.md
@@ -12,7 +12,9 @@ CONTENTS OF THIS FILE
   UPDATES
 ------------
 
-21.12.2021 - added generation graphql schemas (<b>ONLY TYPES</b>) based on <a href="https://github.com/laravel/laravel">Laravel</a> models and tables.
+2021-12-21 - added generation graphql schemas (<b>ONLY TYPES</b>) based on <a href="https://github.com/laravel/laravel">Laravel</a> models and tables.
+2023-09-10 - upgraded to lighthouse 6.16.
+2024-02-03 - Added initial support for generating (<b>QUERIES & MUTATIONS</b>).
  
   DESCRIPTION
 ------------
--- src/Parsers/ModelParser.php
+++ src/Parsers/ModelParser.php
@@ -4,7 +4,9 @@ namespace Nodesol\LighthouseSchemaGenerator\Parsers;
 
 use Exception;
 use ReflectionMethod;
+use ReflectionObject;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use Nodesol\LighthouseSchemaGenerator\Helpers\Reflection;
 
 class ModelParser
@@ -20,11 +22,36 @@ class ModelParser
      */
     public function parse(Model $model): string
     {
+        $reflector  = $this->reflection->reflectionObject($model);
+        $columns    = $this->columnsParser->parse($model);
+        $data       = $this->parseType($model, $reflector, $columns);
+        $data       .= $this->parseQuery($model, $reflector, $columns);
+        $data       .= $this->parseMutations($model, $reflector, $columns);
+
+        return $data;
+    }
+
+    /**
+     * @param Model $model
+     * @param ReflectionObject $reflector
+     * @param string $columns
+     *
+     * @return string
+     */
+    private function parseType(Model $model, ReflectionObject $reflector, string $columns) {
+        $hidden = $model->getHidden();
+        $cols = "";
+        foreach(explode("\n", $columns) as $column) {
+            preg_match("/^\s*(.*?):.*$/", $column, $colname);
+            if(isset($colname[1]) && !in_array($colname[1], $hidden)) {
+                $cols .= $column . "\n";
+            }
+        }
+
         $data      = '';
-        $reflector = $this->reflection->reflectionObject($model);
 
         $data .= "type {$reflector->getShortName()} {\n";
-        $data .= $this->columnsParser->parse($model);
+        $data .= $cols;
 
         $publicMethods = $reflector->getMethods(ReflectionMethod::IS_PUBLIC);
 
@@ -37,6 +64,83 @@ class ModelParser
         }
 
         $data .= '}';
+
+        return $data;
+    }
+
+    /**
+     * @param Model $model
+     * @param ReflectionObject $reflector
+     * @param string $columns
+     *
+     * @return string
+     */
+    private function parseQuery(Model $model, ReflectionObject $reflector, string $columns) {
+        $hidden = $model->getHidden();
+        $fillable = $model->getFillable();
+        $find = "";
+        $where = "";
+        $columns = str_replace("!", "", $columns);
+        foreach(explode("\n", $columns) as $column) {
+            preg_match("/^\s*(.*?):.*$/", $column, $colname);
+            if(isset($colname[1]) && in_array($colname[1], $fillable) && !in_array($colname[1], $hidden)) {
+                $find .= $column . " @eq \n    ";
+                $where .= $column . " @where(operator: \"like\") \n    ";
+            }
+        }
+        $find = trim($find);
+        $where = trim($where);
+        $name = Str::snake($reflector->getShortName());
+        $plural = Str::plural($name);
+
+        $columns = str_ireplace("\n", " @eq \n    ", $columns);
+        $columns2 = str_ireplace("@eq", " @where(operator: \"like\") \n    ", $columns);
+        $data = <<<ENDDATA
+
+        extend type Query {
+            "Find a $name by an identifying attribute."
+            $name(
+                "Search by primary key or column."
+                id: ID @eq
+                $find
+
+            ): {$reflector->getShortName()} @find
+
+            "List multiple $name."
+            $plural(
+                $where
+            ): [{$reflector->getShortName()}!]! @paginate(defaultCount: 10)
+        }
+        ENDDATA;
+
+        return $data;
+    }
+
+    /**
+     * @param Model $model
+     * @param ReflectionObject $reflector
+     * @param string $columns
+     *
+     * @return string
+     */
+    private function parseMutations(Model $model, ReflectionObject $reflector, string $columns) {
+        $fillable = $model->getFillable();
+        $colarray = [];
+        foreach(explode("\n", $columns) as $column) {
+            preg_match("/^\s*(.*?):.*$/", $column, $colname);
+            if(isset($colname[1]) && in_array($colname[1], $fillable)) {
+                $colarray[] = trim($column);
+            }
+        }
+        $columns = implode(", ", $colarray);
+        $data = <<<ENDDATA
+
+        extend type Mutation {
+            create{$reflector->getShortName()}($columns): {$reflector->getShortName()} @create
+            update{$reflector->getShortName()}(id: ID, $columns): {$reflector->getShortName()} @update
+            delete{$reflector->getShortName()}(id: ID): {$reflector->getShortName()} @delete
+        }
+        ENDDATA;
 
         return $data;
     }
